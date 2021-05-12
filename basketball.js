0ed1a8fd8d265c23582b4e159365e4773027f2db
amass156 <massiah156@gmail.com>
2021-05-12 11:02:34 -0400

    MVP all completed..game is completely functional

--- basketball.js
+++ basketball.js
@@ -1,13 +1,44 @@
+// Randomizes the questions
+let  question = document.querySelectorAll("main")
+// console.log(question)
+let questionArray = Array.from(question)
+let copyArr = questionArray.slice(0)
+let answeredQuestion = []
+console.log(copyArr)
+for (let i = 0; i < copyArr.length; i++){
+    let currNum = copyArr[i]
+    // answeredQuestion.push(currNum)
+    // console.log(answeredQuestion)
+    // copyArr.shift()
+    // console.log(copyArr)
+    // answeredQuestion.splice(1,2,"may")
+    // console.log(answeredQuestion)
+}
+// slice to copy array
+// randomizing
+// splice when button is clicked (index number)
+let randomArr = copyArr.sort(() => Math.random() * copyArr.length)
+// console.log(randomArr)
+function removeInvis() {
+        copyArr.sort(() => Math.random() - .5)
+        randomArr[0].classList.remove("invis")
+    }
+    // console.log(removeInvis(questionArray))
+    removeInvis()
+    // console.log(questionArray)
+
 let score = 0
 
 //click on the correct answer, and the score goes up by one.
 let correctButton = document.querySelectorAll(".correct")
-// console.log(correctButton);
+// console.log(correctButton)
+function scoreIncrease(){
+    score += 1
+    document.getElementById("score").innerHTML = score;
+}
+
 for (let i = 0; i < correctButton.length; i++){
-    correctButton[i].addEventListener("click",function() {
-        score += 1
-        document.getElementById("score").innerHTML = score;
-    })
+    correctButton[i].addEventListener("click", scoreIncrease)  
 }
 
 
@@ -28,67 +59,76 @@ for (let i = 0; i < buttonClick.length; i++){
 }
 
 
-
 //Next question appears when a button is clicked
 let button = document.querySelectorAll(".btn")
-correct = document.querySelectorAll("correct")
-let  question = document.querySelectorAll("main")
-console.log(question);
-// console.log(next);
+let correct = document.querySelectorAll("correct")
+// let  question = document.querySelectorAll("main")
+
 let currClick = 0
+let answeredQuestions = []
 
 function nextPage(){
     console.log(button);
     button.forEach(e => 
         e.addEventListener("click", function(){
-            question[currClick].classList.add("invis")
+            // this will contain answered questions
+            copyArr[currClick].classList.add("invis")
             currClick += 1
+            answeredQuestions.push(e)
+            console.log(answeredQuestions)
             console.log(e);
-            // if(question.length === currClick){
-    
-                // }
-                question[currClick].classList.remove("invis")
+            if(question.length === currClick){
+                let myStorage = window.localStorage
+                myStorage.setItem("score", score )
+                console.log(myStorage);
+                window.location.href = "score.html"
+            }
+            copyArr[currClick].classList.remove("invis")
+            // copyArr.shift()
+            console.log(copyArr)
         }))
 }
 nextPage()
 
-
-
-//skip function 
-
-let skip = document.querySelectorAll(".skip")
-let currQ = 0
-// let  question = document.querySelectorAll("main")
-
-function skipButton(){
-    // console.log(skip);
-    skip.forEach(e =>  
-        e.addEventListener("click", function(){
-            question[currQ].classList.add("invis")
-            currQ += 1
-            if(question.length === currQ){
-                window.location.href = "last page goes here"
-            }
-            question[currQ].classList.remove("invis")
-        }))
-}
-skipButton()
-
-// let test = document.querySelectorAll(".skip")
 // function nextQuestion(){
-//     for (let i = 0; i < test.length ; i++){
-//         console.log(test);
-//         next[i].addEventListener("click", function(){ 
+//     for(let i = 0; i < button.length; i++){
+//         button[i].addEventListener("click", function() {
 //             question[currClick].classList.add("invis")
-//                 currClick =+ 1
-//                 if(question.length === currClick){
-        
+//             currClick += 1
+//             // console.log(e);
+//             if(question.length === currClick){
+//                 let myStorage = window.localStorage
+//                 myStorage.setItem("score", score )
+//                 console.log(myStorage);
+//                 window.location.href = "score.html"
 //                 }
 //                 question[currClick].classList.remove("invis")
 //         })
 //     }
 // }
 // nextQuestion()
+
+
+
+//skip function 
+let skip = document.querySelectorAll(".skip")
+// let  question = document.querySelectorAll("main")
+
+function skipButton(){
+    // console.log(skip);
+    skip.forEach(e =>  
+        e.addEventListener("click", function(){
+            question[currClick].classList.add("invis")
+            currClick += 1
+            if(question.length === currClick){
+                window.location.href = "score.html"
+            }
+            question[currClick].classList.remove("invis")
+        }))
+}
+skipButton()
+
+
 
 
 
--- football.js
+++ football.js
@@ -1,3 +1,14 @@
+let  question = document.querySelectorAll("main")
+let questionArray = Array.from(question)
+let copyArr = questionArray.slice(0)
+let randomArr = copyArr.sort(() => Math.random() * copyArr.length)
+
+function randomQuestion() {
+    copyArr.sort(() => Math.random() - .5)
+    randomArr[0].classList.remove("invis")
+}
+randomQuestion(questionArray)
+
 // Allow each button box highlights a certain color when clicked
 let currButton = null
 // let buttonClick = document.getElementsByClassName("btn")
@@ -9,67 +20,52 @@ for (let i = 0; i < button.length; i++){
             currButton.style.backgroundColor = ""
         }
         button[i].style.backgroundColor = "red"
-        // if(button[i].classList[1] === "correct"){
-        //     score += 1
-        //     document.getElementById("score").innerHTML = score;
-        // }        currButton = button[i]
-        console.log(currButton);
+        currButton = button[i]
+        if(button[i].classList[1] === "correct"){
+            score += 1
+            console.log(currButton);
+        }
     })
 }
 //Next question appears when a button is clicked and score goes up by one
 let nextButton = document.querySelectorAll(".next")
 let correctButton = document.querySelectorAll(".correct")
 // console.log(correctButton);
-let  question = document.querySelectorAll("main")
-// console.log(question);
-// console.log(next);
+
 let currClick = 0
 let score = 0
 
 
-// if(correctButton === "click"){
-//     score += 1
-//     document.getElementById("score").innerHTML = score;
-// }
-// function plusOne(){
-// }
-
 function nextPage(){
     nextButton.forEach(e => 
         e.addEventListener("click", function(){
-            question[currClick].classList.add("invis")
+            copyArr[currClick].classList.add("invis")
             currClick += 1
-            console.log(e);
-            
-            question[currClick].classList.remove("invis")
-        
-            for(let i = 0; i < correctButton.length; i++){
-                console.log(button[i].classList);
-                if(correctButton[i].classList.value.includes("correct")){
-                    score += 1
-                    document.getElementById("score").innerHTML = score;
-                }
-            }
+            if(question.length === currClick){
+                window.location.href = "score.html"
+            }            
+            copyArr[currClick].classList.remove("invis")
+            document.getElementById("score").innerHTML = score;
         }))  
 }
 nextPage()
 
 
 
+
+
 //skip function 
 
 let skip = document.querySelectorAll(".skip")
-let currQ = 0
 // let  question = document.querySelectorAll("main")
 
 function skipButton(){
-    // console.log(skip);
     skip.forEach(e =>  
         e.addEventListener("click", function(){
-            question[currQ].classList.add("invis")
-            currQ += 1
-            if(question.length === currQ){
-                window.location.href = "last page goes here"
+            question[currClick].classList.add("invis")
+            currClick += 1
+            if(question.length === currClick){
+                window.location.href = "score.html"
             }
             question[currQ].classList.remove("invis")
         }))
@@ -79,18 +75,7 @@ skipButton()
 
 
 
-// let score = 0
 
-//click on the correct answer, and the score goes up by one.
-// let correctButton = document.querySelectorAll(".correct")
-// console.log(button[0].classList);
-// for (let i = 0; i < correctButton.length; i++){
-//     correctButton[i].addEventListener("click",function() {
-//         score += 1
-//         document.getElementById("score").innerHTML = score;
-//     })
-
-// }
 
 
 
