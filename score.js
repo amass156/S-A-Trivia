4a7bd41b74957b81cee3db0f15af08e751fd86f8
amass156 <massiah156@gmail.com>
2021-05-14 19:22:56 -0400

    committing to include 3 changes to master from main

--- score.js
+++ score.js
@@ -5,3 +5,9 @@ console.log(myStorage);
 text = document.querySelector(".final-score")
 finalScore = document.createTextNode(`Your Final Score is ${finalScore}!`)
 text.appendChild(finalScore)
+
+// Allow user to start from welcome page
+let playAgain = document.querySelector(".replay")
+playAgain.addEventListener("click", function(){
+    window.location.href = "welcome.html"
+})
